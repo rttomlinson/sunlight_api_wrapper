5615da73ff213b7e3845ea488cee6d998b9e0971
Renzo Tomlinson <rttomlinson@gmail.com>
2017-02-26 06:36:32 +0000

    Add wrapRequestWithPromise helper to index.js. Add getMostRcentVotes function to Sunlight_API_Wrapper

--- index.js
+++ index.js
@@ -3,46 +3,60 @@
 
 const request = require("request");
 
-let wrapper_methods = {};
-let options = {};
+let customRequest = wrapRequestWithPromise(request.get);
 
-const baseUrl = "https://congress.api.sunlightfoundation.com/";
-let qs = {}; //reference to the object at options property qs (query strings)
-options["qs"] = qs; //Add qs to options object
+class SunlightAPIWrapper {
+    constructor() {
+        this.baseUrl = "https://congress.api.sunlightfoundation.com/";
 
-
-
-
-function getRepsByZipcode(zipcode) { //This returns a promise
-    qs.zip = zipcode;
-    options["uri"] = `${baseUrl}legislators/locate/`;
-    options.qs["fields"] = "chamber,aliases,party,bioguide_id,district";
-    return new Promise(function (resolve, reject) {
-        request.get(options, callbackForSunlight(resolve, reject));
+    }
+    
+    getRepsByZipcode(zipcode) { //This returns a promise
+        let options = {};
+        options.qs = {};
+        options.qs["zip"] = zipcode;
+        options["uri"] = `${this.baseUrl}legislators/locate/`;
+        options.qs["fields"] = "chamber,aliases,party,bioguide_id,district";
+        return customRequest(options);
+            
+    }
+    
+    getMostRecentVotes(bioguide_id) {
+        let options = {};
+        options.qs = {};
+        options["uri"] = `${this.baseUrl}votes/`;
+        options.qs["fields"] = `voter_ids.${bioguide_id},voted_at,bill.bill_id,bill.official_title`; //returns, bioguide_ids vote, vote time, bill id, and bill official title
+        options.qs[`voter_ids.${bioguide_id}__exists`] = "true"; //check that this rep voted
+        options.qs["order"] = "voted_at__desc"; //order votes chronologically
+        options.qs["bill_id__exists"] = "true"; //check that this vote is regarding a bill
+        return customRequest(options);
         
-    });
+    }
 }
 
+module.exports = SunlightAPIWrapper;
 
-/* Helper function */
-function callbackForSunlight(resolve, reject) {
-    return function (error, response, body) {
-            if (error) {
-                reject(error);
-            }
-            else if (response.statusCode == 200) {
-                resolve(JSON.parse(body));
-                
-            }
-            else {
-                reject(`Status code is ${response.statusCode}`);
-            }
+/*This uses the special callback for request module http calls*/
+
+function wrapRequestWithPromise(fn) {
+    return function() {
+        let args = [].slice.call(arguments);
+        
+        return new Promise(function(resolve, reject) {
+            fn.apply(
+                null,
+                args.concat(function (error, response, body){
+                    if (error) {
+                        reject(error);
+                    }
+                    else if (response.statusCode == 200) {
+                        resolve(JSON.parse(body));
+                    }
+                    else {
+                        reject(`Status code is ${response.statusCode}`);
+                    }
+                })
+            );
+        });
     };
 }
-
-
-wrapper_methods.getRepsByZipcode = getRepsByZipcode;
-
-
-
-module.exports = wrapper_methods;
--- test_file.js
+++ test_file.js
@@ -1,16 +1,148 @@
 "use strict";
 
-const sunlight = require("./index.js");
+const Sunlight = require("./index.js");
+let sunlight  = new Sunlight();
+
+
+
+/* ------------------------------------- Get Reps by Zipcode ------------------------------- */
 
 
 let repsByZip = sunlight.getRepsByZipcode(75074); //method falls return promises
 
-
 repsByZip.then(function onFulfill(data) { //expect data to be a JS object
     console.log(data);
+    let prettyData = {
+        "senate" : [],
+        "house" : []
+    };
     data.results.forEach(function (element, index, arr) {
-        console.log(element.aliases[0]);
+        let cleanedData = cleanRepData(element);
+        if (element.chamber == "senate") {
+            prettyData.senate.push(cleanedData);
+        } else {
+            prettyData.house.push(cleanedData);
+        }
     });
+    console.log(prettyData);
 }).catch(function onError(error) {
     console.log(error);
 });
+
+
+
+/* ------------- repVote calls ----------------------------------*/
+
+
+let repsVotes = sunlight.getMostRecentVotes("J000174");
+
+repsVotes.then(function onFulfill(data) {
+    console.log(cleanVoteData(data));
+
+}).catch(function onError(error) {
+    console.log(error);
+});
+
+
+
+
+
+/* ----------------------- DATA INFO ------------------------------ */
+
+
+/*Pretty data for reps by zipcode should follow the schema of:
+{
+    "house" : [
+        {
+            "fullName" : [string],
+            "party" : [string],
+            "bioguideId : [string],
+            "district" : [number]
+        },...
+    ],
+    "senate" : [
+        {
+            "fullName" : [string],
+            "party" : [string],
+            "bioguideId : [string],
+        }
+    
+    
+    
+    ]
+}
+*/
+function cleanRepData(rep) {
+    let cleanData = {};
+    cleanData.fullName = rep.aliases[0];
+    cleanData.party = rep.party;
+    cleanData.bioguideId = rep.bioguide_id;
+    if (rep.chamber == "house") {
+        cleanData.district = rep.district;
+    }
+    return cleanData;
+}
+
+
+/*---------------------------------Voting Records ------------------------------*/
+
+/* -------------------------------Raw Data -----------------------------*/
+/*
+{
+    "results" : [
+        {
+            "bill": {
+                "bill_id" : [string],
+                "official_title" : [string]
+            },
+            "voted_at" : [string],
+            "voter_ids" : {
+                "[bioguide_id]" : [string]
+            }
+        },...
+        
+    
+    
+    
+    ],
+    "count" : [number],
+    "page" : [Object]
+}
+
+
+*/
+
+
+
+
+
+
+/*
+Pretty data for voting records
+
+[
+    {
+        "bill_id" : [string],
+        "bill_title" : [string],
+        "recorded-vote": [string]
+    },...
+
+
+
+]
+
+
+*/
+function cleanVoteData(data) {
+    let cleanedData = [];
+    data.results.forEach(function (element, index, arr) {
+        let holderObj = {};
+        holderObj["bill_id"] = element.bill.bill_id;//billid from element
+        holderObj["bill_title"] = element.bill.official_title;
+        holderObj["recorded-vote"] = element.voter_ids[Object.getOwnPropertyNames(element.voter_ids)[0]];
+        cleanedData.push(holderObj);
+    });
+    return cleanedData;
+}
+
+
